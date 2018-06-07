838abd5a80684b7a60299ea7e76bcc8759815463
Nicolas <nicolas.charvoz@epitech.eu>
2018-06-07 10:59:38 -0700

    Currency Detail skeleton

--- Coinlend/screens/BotDetail.js
+++ Coinlend/screens/BotDetail.js
@@ -1,182 +1,187 @@
 import React, { Component } from 'react';
-import { Image, View, Text, FlatList, ActivityIndicator, SafeAreaView } from 'react-native';
+import { Image, View, Text, FlatList, ActivityIndicator, SafeAreaView, TouchableOpacity } from 'react-native';
 import { uniqueId } from 'lodash-es';
 
 class BotDetailListItem extends React.PureComponent {
-
-	render() {
-
-		return (
-			<View backgroundColor='#151F29'>
-						<View
-							style={{flex: 1,
-								height: 50,
-								flexDirection: 'row',
-								backgroundColor: '#151F29',
-								justifyContent: 'center',
-								alignItems: 'center'}}>
-								<View style={{flex: 1, height: this.cellHeight}}>
-									<Text style={{ color: 'white', textAlign: 'center', lineHeight: 50}}>
-										{this.props.item.coin}
-									</Text>
-								</View>
-								<View style={{flex: 1, height: this.cellHeight}}>
-									<Text style={{ color: 'white', textAlign: 'center', lineHeight: 50}}>
-										{this.props.item.isLending}
-									</Text>
-								</View>
-								<View style={{flex: 1, height: this.cellHeight}}>
-									<Text style={{ color: 'white', textAlign: 'center', lineHeight: 50}}>
-										{this.props.item.amountReserved}
-									</Text>
-								</View>
-								<View style={{flex: 1, height: this.cellHeight}}>
-									<Text style={{ color: 'white', textAlign: 'center', lineHeight: 50}}>
-										{this.props.item.minRate}
-									</Text>
-								</View>
-								<View style={{flex: 1, height: this.cellHeight}}>
-									<Text style={{ color: 'white', textAlign: 'center', lineHeight: 50}}>
-										{this.props.item.minDuration}
-									</Text>
-								</View>
-							</View>
-						</View>
-					);
-	}
-}
-
-class BotDetailFlatList extends React.Component {
-
-	_keyExtractor = (item, index) => _.uniqueId();
-
-	_renderSeparator = () => {
-		return (
-			<View
-				style={{ height: 1, width: '86%', backgroundColor: 'black', marginLeft: '14%' }}
-			/>
-		);
-	}
-
-	_renderHeader = () => {
-		return (
-			<View backgroundColor='#151F29'>
-						<View
-							style={{flex: 1,
-								height: 40,
-								flexDirection: 'row',
-								backgroundColor: '#27292A',
-								justifyContent: 'center',
-								alignItems: 'center'}}>
-								<View style={{flex: 1, height: this.cellHeight}}>
-									<Text style={{ color: 'white', textAlign: 'center', lineHeight: 40}}>
-										Coin
-									</Text>
-								</View>
-								<View style={{flex: 1, height: this.cellHeight}}>
-									<Text style={{ color: 'white', textAlign: 'center', lineHeight: 40}}>
-										Lend
-									</Text>
-								</View>
-								<View style={{flex: 1, height: this.cellHeight}}>
-									<Text style={{ color: 'white', textAlign: 'center', lineHeight: 40}}>
-										Reserved
-									</Text>
-								</View>
-								<View style={{flex: 1, height: this.cellHeight}}>
-									<Text style={{ color: 'white', textAlign: 'center', lineHeight: 40}}>
-										Min rate
-									</Text>
-								</View>
-								<View style={{flex: 1, height: this.cellHeight}}>
-									<Text style={{ color: 'white', textAlign: 'center', lineHeight: 40}}>
-										Dur (%)
-									</Text>
-								</View>
-							</View>
-						</View>
-					);
-				}
-
-	_renderItem = ({ item }) => (
-		<BotDetailListItem item={item} containerStyle={{ borderBottomWidth: 0 }}/>
-	);
-
-	constructor(props) {
-		super(props);
-
-		this.state = {
-			data: [],
-			isLoading: true
-		}
-	}
-
-	componentDidMount() {
-		this.setState({
-			isLoading: false
-		})
-
-		list = []
-
-		for (let i = 0; i < 100; i ++) {
-			list.push({
-				coin: 'USD',
-				isLending: true,
-				amountReserved: 0.6,
-				minRate: 0.2,
-				minDuration: 2,
-				maxDuration: 30,
-				threshold: false,
-				iceberg: true,
-				icebergAmount: 0
-			})
-
-			this.setState({
-				data: list
-			})
-		}
+	_onPress(id) {
+		this.props.navigation.navigate('CurrencyDetail')
 	}
 
 	render() {
-		if (this.state.isLoading) {
-			return (
-				<SafeAreaView backgroundColor='#27292A' style={{flex: 1, height: '100%', justifyContent: 'center', alignItems:'center'}}>
-					<ActivityIndicator size="large" color="#000000" />
-				</SafeAreaView>
+
+		return (
+			<TouchableOpacity onPress={() => this._onPress(this.props.item.id)}>
+				<View backgroundColor='#151F29'>
+					<View
+						style={{flex: 1,
+							height: 50,
+							flexDirection: 'row',
+							backgroundColor: '#151F29',
+							justifyContent: 'center',
+							alignItems: 'center'}}>
+							<View style={{flex: 1, height: this.cellHeight}}>
+								<Text style={{ color: 'white', textAlign: 'center', lineHeight: 50}}>
+									{this.props.item.coin}
+								</Text>
+							</View>
+							<View style={{flex: 1, height: this.cellHeight}}>
+								<Text style={{ color: 'white', textAlign: 'center', lineHeight: 50}}>
+									{this.props.item.isLending}
+								</Text>
+							</View>
+							<View style={{flex: 1, height: this.cellHeight}}>
+								<Text style={{ color: 'white', textAlign: 'center', lineHeight: 50}}>
+									{this.props.item.amountReserved}
+								</Text>
+							</View>
+							<View style={{flex: 1, height: this.cellHeight}}>
+								<Text style={{ color: 'white', textAlign: 'center', lineHeight: 50}}>
+									{this.props.item.minRate}
+								</Text>
+							</View>
+							<View style={{flex: 1, height: this.cellHeight}}>
+								<Text style={{ color: 'white', textAlign: 'center', lineHeight: 50}}>
+									{this.props.item.minDuration}
+								</Text>
+							</View>
+						</View>
+					</View>
+				</TouchableOpacity>
 			);
 		}
-		return (
-			<SafeAreaView backgroundColor='#27292A'>
-				<FlatList
-					style={{backgroundColor: '#171F27'}}
-					data={this.state.data}
-					keyExtractor={this._keyExtractor}
-					renderItem={this._renderItem}
-					ListHeaderComponent={this._renderHeader}
-					ItemSeparatorComponent={this._renderSeparator}
+	}
+
+	class BotDetailFlatList extends React.Component {
+
+		_keyExtractor = (item, index) => _.uniqueId();
+
+		_renderSeparator = () => {
+			return (
+				<View
+					style={{ height: 1, width: '86%', backgroundColor: 'black', marginLeft: '14%' }}
 				/>
-		</SafeAreaView>
-		);
-	}
-}
+			);
+		}
+
+		_renderHeader = () => {
+			return (
+				<View backgroundColor='#151F29'>
+					<View
+						style={{flex: 1,
+							height: 40,
+							flexDirection: 'row',
+							backgroundColor: '#27292A',
+							justifyContent: 'center',
+							alignItems: 'center'}}>
+							<View style={{flex: 1, height: this.cellHeight}}>
+								<Text style={{ color: 'white', textAlign: 'center', lineHeight: 40}}>
+									Coin
+								</Text>
+							</View>
+							<View style={{flex: 1, height: this.cellHeight}}>
+								<Text style={{ color: 'white', textAlign: 'center', lineHeight: 40}}>
+									Lend
+								</Text>
+							</View>
+							<View style={{flex: 1, height: this.cellHeight}}>
+								<Text style={{ color: 'white', textAlign: 'center', lineHeight: 40}}>
+									Reserved
+								</Text>
+							</View>
+							<View style={{flex: 1, height: this.cellHeight}}>
+								<Text style={{ color: 'white', textAlign: 'center', lineHeight: 40}}>
+									Min rate
+								</Text>
+							</View>
+							<View style={{flex: 1, height: this.cellHeight}}>
+								<Text style={{ color: 'white', textAlign: 'center', lineHeight: 40}}>
+									Dur (%)
+								</Text>
+							</View>
+						</View>
+					</View>
+				);
+			}
+
+			_renderItem = ({ item }) => (
+				<BotDetailListItem item={item} navigation={this.props.navigation} containerStyle={{ borderBottomWidth: 0 }}/>
+			);
+
+			constructor(props) {
+				super(props);
+
+				this.state = {
+					data: [],
+					isLoading: true
+				}
+			}
+
+			componentDidMount() {
+				this.setState({
+					isLoading: false
+				})
+
+				list = []
+
+				for (let i = 0; i < 100; i ++) {
+					list.push({
+						coin: 'USD',
+						isLending: true,
+						amountReserved: 0.6,
+						minRate: 0.2,
+						minDuration: 2,
+						maxDuration: 30,
+						threshold: false,
+						iceberg: true,
+						icebergAmount: 0
+					})
+
+					this.setState({
+						data: list
+					})
+				}
+			}
+
+			render() {
+				if (this.state.isLoading) {
+					return (
+						<SafeAreaView backgroundColor='#27292A' style={{flex: 1, height: '100%', justifyContent: 'center', alignItems:'center'}}>
+							<ActivityIndicator size="large" color="#000000" />
+						</SafeAreaView>
+					);
+				}
+				return (
+					<SafeAreaView backgroundColor='#27292A'>
+						<FlatList
+							style={{backgroundColor: '#171F27'}}
+							data={this.state.data}
+							keyExtractor={this._keyExtractor}
+							renderItem={this._renderItem}
+							ListHeaderComponent={this._renderHeader}
+							ItemSeparatorComponent={this._renderSeparator}
+						/>
+					</SafeAreaView>
+				);
+			}
+		}
 
 
-class BotDetail extends React.Component {
+		class BotDetail extends React.Component {
 
-	static navigationOptions = {
-		headerStyle: {
-			backgroundColor: '#27292A',
-		},
-		headerTitleStyle: {
-			fontWeight: 'bold',
-		},
-	}
-	
-	render() {
-		return (
-				<BotDetailFlatList />
-		);
-	}
-}
+			static navigationOptions = {
+				headerStyle: {
+					backgroundColor: '#27292A',
+				},
+				headerTitleStyle: {
+					fontWeight: 'bold',
+				},
+			}
 
-module.exports = BotDetail;
+			render() {
+				return (
+					<BotDetailFlatList navigation={this.props.navigation}/>
+				);
+			}
+		}
+
+		module.exports = BotDetail;
--- Coinlend/screens/CurrencyDetail.js
+++ Coinlend/screens/CurrencyDetail.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { Switch, FlatList, Dimensions, StyleSheet, SafeAreaView, ActivityIndicator, Linking } from 'react-native';
+import { ScrollView, Switch, FlatList, Dimensions, StyleSheet, SafeAreaView, ActivityIndicator, Linking } from 'react-native';
 import { View, TextInput, Text } from 'react-native-ui-lib';
-import { FormLabel, FormInput, Button } from 'react-native-elements'
+import { FormLabel, FormInput, Button, Slider } from 'react-native-elements'
 import { uniqueId } from 'lodash-es';
 import { StackNavigator } from 'react-navigation';
 
 import APIClient from '../network/APIClient';
+
+const fullWidth = Dimensions.get('window').width;
 
 class CurrencyDetail extends React.Component {
 
@@ -18,12 +20,64 @@ class CurrencyDetail extends React.Component {
 		},
 	}
 
+	constructor(props) {
+		super(props)
+	}
+
 	render() {
 		return (
-				<View>
-					<Text>Hello Currency Detail</Text>
+			<ScrollView overScrollMode='always'
+			contentContainerStyle={{flexGrow: 1, flexDirection: 'column', justifyContent: 'center', alignItems:'center'}}
+			backgroundColor='#171F27'>
+
+				<FormLabel containerStyle={{marginTop: 10}}>Reserved amount: $0.00</FormLabel>
+				<FormInput
+					containerStyle={{marginTop: 10, width: fullWidth*0.5}}
+					style={{height: 45, color: 'white'}}
+					placeholder='Reserved Amount'
+					placeholderTextColor='white'
+					autoCapitalize='none'
+				/>
+
+				<FormLabel containerStyle={{marginTop: 10}}>Minimum interest rate: $0.00</FormLabel>
+				<Slider style={{marginTop: 10, width: fullWidth*0.8}} minimumValue={0} maximumValue={100} value={0} />
+
+				<FormLabel containerStyle={{marginTop: 10}}>Always lend at the minimum duration of 2 days</FormLabel>
+				<Switch style={{marginTop: 10}} value={false}/>
+
+				<FormLabel containerStyle={{marginTop: 10}}>Threshold for max. loan duration:</FormLabel>
+				<Slider style={{marginTop: 10, width: fullWidth*0.8}} minimumValue={0} maximumValue={100} value={0} />
+
+
+				<FormLabel containerStyle={{marginTop: 10, width: fullWidth*0.8}}>Minimum (2 days) and maximum (30 days) loan duration</FormLabel>
+				<View flex={1} flexDirection='row'>
+					<FormInput
+						containerStyle={{marginTop: 10, width: fullWidth*0.3}}
+						style={{height: 45, color: 'white'}}
+						placeholder='Min'
+						placeholderTextColor='white'
+						autoCapitalize='none'
+					/>
+					<FormInput
+						containerStyle={{marginTop: 10, width: fullWidth*0.3}}
+						style={{height: 45, color: 'white'}}
+						placeholder='Max'
+						placeholderTextColor='white'
+						autoCapitalize='none'
+					/>
 				</View>
-		);
+
+				<FormLabel containerStyle={{marginTop: 10}}>Use the Iceberg strategy. Only create loans with maximum amount of: $0.00</FormLabel>
+				<Switch style={{marginTop: 10}} value={false}/>
+				<FormInput
+					containerStyle={{marginTop: 10, width: fullWidth*0.5, marginBottom: 20}}
+					style={{height: 45, color: 'white'}}
+					placeholder='Max'
+					placeholderTextColor='white'
+					autoCapitalize='none'
+				/>
+			</ScrollView>
+			);
 	}
 }
 
